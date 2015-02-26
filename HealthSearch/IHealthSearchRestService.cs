4b4f79fbf2117a5fb0b7ddb5828fe4907189252c
JoaoJoseFaria <jfaria.gmr@gmail.com>
2015-02-26 01:18:15 +0000

    RestService

---
+++ HealthSearch/IHealthSearchRestService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace HealthSearch
+{
+    [ServiceContract]
+    public interface IHealthSearchRestService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "teste")]
+        void teste();
+    }
+}
--- HealthSearch/IHealthSearchSoapService.cs
+++ HealthSearch/IHealthSearchSoapService.cs
@@ -14,28 +14,70 @@ namespace HealthSearch
 
         #region Prestador
 
+        /// <summary>
+        /// Devolve todos os prestadores
+        /// </summary>
+        /// <returns>Lista de prestadores</returns>
         [OperationContract]
         List<Prestador> GetAllPrestador();
 
+        /// <summary>
+        /// Devolve os prestadores por id
+        /// </summary>
+        /// <param name="id">Id do prestador</param>
+        /// <returns>Prestador</returns>
         [OperationContract]
         Prestador GetPrestadorById(string id);
 
+        /// <summary>
+        /// Devolve os prestadores por serviço
+        /// </summary>
+        /// <param name="id">Id do serviço</param>
+        /// <returns>Lista de prestadores</returns>
         [OperationContract]
         List<Prestador> GetPestadorByServico(string id);
 
+        /// <summary>
+        /// Devolve os prestadores pela localização
+        /// </summary>
+        /// <param name="pais">Pais a procurar</param>
+        /// <param name="regiao">Região a procurar</param>
+        /// <param name="distrito">Distrito a procurar</param>
+        /// <param name="concelho">Concelho a procurar</param>
+        /// <returns>Lista de prestadores</returns>
         [OperationContract]
         List<Prestador> GetPestadorByLocalizacaoo(string pais = null, string regiao = null,
                             string distrito = null, string concelho = null);
 
+        /// <summary>
+        /// Actualiza dados do prestador
+        /// </summary>
+        /// <param name="prestador">Prestador actualizado</param>
+        /// <returns>True caso actualiza, false caso contrário</returns>
         [OperationContract]
         bool UpdatePrestador(Prestador prestador);
 
+        /// <summary>
+        /// Marca prestador como eliminado
+        /// </summary>
+        /// <param name="id">Id do restador a marcar</param>
+        /// <returns>True caso actualiza, false caso contrário<</returns>
         [OperationContract]
         bool DeletePrestador(string id);
 
+        /// <summary>
+        /// Elimina registo do prestador da base de dados
+        /// </summary>
+        /// <param name="id">Id do prestador a eliminar</param>
+        /// <returns>True caso elimina, false caso contrário<</returns>
         [OperationContract]
         bool HardDeletePrestador(string id);
 
+        /// <summary>
+        /// Adiciona novo prestador
+        /// </summary>
+        /// <param name="prestador">Prestador a adicionar</param>
+        /// <returns>Id novo prestador</returns>
         [OperationContract]
         int AddPrestador(Prestador prestador);
 
