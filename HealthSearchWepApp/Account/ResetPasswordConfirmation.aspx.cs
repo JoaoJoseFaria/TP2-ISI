9dd983759725e1a8a2cb3b72e2a51c048ecaa771
JoaoJoseFaria <jfaria.gmr@gmail.com>
2015-03-12 22:48:42 +0000

    WebService

--- HealthSearchWepApp/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace HealthSearchWepApp.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- HealthSearchWepApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HealthSearchWepApp.Global" Language="C#" %>
